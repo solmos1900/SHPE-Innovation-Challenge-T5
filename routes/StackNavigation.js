eb3f77edb47b524421270babc8e687804b0dd81f
Khai Pham <zekhai@mail.fresnostate.edu>
2020-10-22 20:22:05 -0700

    merge complete

--- routes/StackNavigation.js
+++ routes/StackNavigation.js
@@ -1,21 +1,11 @@
 import * as React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
+import { Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import Landing from '../screens/Landing/scene';
+import Register from '../screens/Register/scene';
 import LoginScreen from '../screens/Login/login';
-// import LandingScreen from '../Screen/landing';
-//import HomeScreen from '../Screen/HomeScreen';
-import NotificationScreen from '../screens/Messages/NotificationScreen';
-import MapScreen from '../screens/MapScreen';
 import BottomTabNav from './BottomTabNavigator';
-import GooglePlacesInput from '../screens/Google Maps/GooglePlacesTest';
-import {
-  AsyncStorage,
-  Button,
-  Text,
-  TextInput,
-  View,
-  StyleSheet,
-} from 'react-native';
 
 const Stack = createStackNavigator();
 
@@ -26,45 +16,13 @@ function SplashScreen() {
     </View>
   );
 }
-export default function Navigation({ navigation }) { 
-  // const [state, dispatch] = React.useReducer(
-  //   (prevState, action) => {
-  //     switch (action.type) {
-  //       case 'RESTORE_TOKEN':
-  //         return {
-  //           ...prevState,
-  //           userToken: action.token,
-  //           isLoading: false,
-  //         };
-  //       case 'SIGN_IN':
-  //         return {
-  //           ...prevState,
-  //           isSignout: false,
-  //           userToken: action.token,
-  //         };
-  //       case 'SIGN_OUT':
-  //         return {
-  //           ...prevState,
-  //           isSignout: true,
-  //           userToken: null,
-  //         };
-  //     }
-  //   },
-  //   {
-  //     isLoading: false,
-  //     isSignout: false,
-  //     userToken: null,
-  //   }
-  // );
+export default function Navigation() { 
   return (
     <Stack.Navigator initialRouteName='Landing'>
       <Stack.Screen name='Landing' component={Landing} options={{ headerShown: false }} />
       <Stack.Screen name='Register' component={Register} />
       <Stack.Screen name='Login' component={LoginScreen} />
       <Stack.Screen name='Home' component={BottomTabNav} options={{ headerShown: false }} />
-      <Stack.Screen name="Notification" component={NotificationScreen} />
-      <Stack.Screen name="Map" component={MapScreen} />
-      <Stack.Screen name="GoogleTest" component={GooglePlacesInput} />
     </Stack.Navigator>
   );
 }
